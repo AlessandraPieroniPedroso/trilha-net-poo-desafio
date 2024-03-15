e63ca120ea72f905195e55293a191fef8ea92f3f
Alessandra Pieroni Pedroso <alepieronif@gmail.com>
2024-03-15 13:53:37 -0300

    Implementando as classes Iphone e Nokia

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,26 @@
 namespace DesafioPOO.Models
-{
+{   
+    // TODO: Implementar as propriedades faltantes de acordo com o diagrama
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
 
-        public Smartphone(string numero)
+        private int Memoria { get; set; }
+
+        
+
+        // TODO: Passar os parâmetros do construtor para as propriedades
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
+            
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+
+            
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Nokia nokia = new Nokia("1234567", "Nokia 1010", "Imei 12345", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatapps");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Iphone iphone = new Iphone("1234567", "Iphone 13", "Imei 12345", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+
+
+
